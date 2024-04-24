8321f0c01370d1c459279f5c07f9335ca7d9075f
ooylf <60663156+ooylf@users.noreply.github.com>
2024-04-24 18:51:17 +0900

    [240424] S3L5 수강

--- Section3/code/index.js
+++ Section3/code/index.js
@@ -1,7 +1,11 @@
 // const {add, sub} = require("./math");
 import mul, { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
+import randomColor from "randomcolor";
 
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
 // console.log(moduleData);
