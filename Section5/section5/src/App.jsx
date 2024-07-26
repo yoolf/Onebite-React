d885730117d016f7138fc03e9c92cdf3fc081f73
ooylf <118517380+yoolf@users.noreply.github.com>
2024-07-27 00:32:00 +0900

    [240726] S5까지 수강 완료

--- Section5/section5/src/App.jsx
+++ Section5/section5/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
+import Register from './components/Register';
 // import { useState } from "react";
 // import HookExam from './components/HookExam';
-import Bulb from './components/Bulb';
+// import Bulb from './components/Bulb';
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <Bulb />
-      {/* <HookExam /> */}
+      <Register />
     </>
   );
 }
--- Section5/section5/src/components/Register.jsx
+++ Section5/section5/src/components/Register.jsx
@@ -1,29 +1,50 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Register = () => {
-
     const [input, setInput] = useState({
-        name: '',
-        birth: '',
-        country: ''
-    })
+        name: "",
+        birth: "",
+        country: "",
+        bio: "",
+    });
 
-    console.log(input);
+    const countRef = useRef(0);
+    let count = 0;
+    // 렌더링은 한번뿐
+    // console.log("Register 렌더링");
+
+    // input 태그가 렌더링하는 DOM 요소가 inputRef라는 레퍼런스 오브젝트에 저장된다
+    const inputRef = useRef();
 
     const onChange = (e) => {
+        countRef.current++;
+        count++;
+        console.log(countRef.current, count);
+        // onChange에 따라 Register 함수가 매번 호출되는데 그 때마다 내부가 초기화 된다 따라서 count는 항상 1이다
+        // 함수 내에서 사용하는 변수를 외부에 선언하면 컴포넌트가 중복 사용될 때 변수가 공유되기 때문에 올바르지 않다
+
+        console.log(e.target.name, e.target.value);
         setInput({
             ...input,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
         });
+    };
+
+    const onSubmit = () => {
+        if(input.name === "") {
+            // 이름 입력 DOM 요소에 포커스한다
+            inputRef.current.focus();
+        }
     };
 
     return (
         <div>
             <div>
                 <input 
+                    ref={inputRef}
                     name='name' 
                     value={input.name} 
-                    onChange={onChange} placeholder="name" 
+                    onChange={onChange} placeholder={"name"} 
                 />
             </div>
 
@@ -32,7 +53,7 @@ const Register = () => {
                     name="birth"
                     value={input.birth}
                     onChange={onChange}
-                    type="date"
+                    type={"date"}
                 />
             </div>
 
@@ -46,7 +67,11 @@ const Register = () => {
                     <option value="us">USA</option>
                 </select> 
             </div>
+            <div>
+                <textarea name="bio" value={input.bio} onChange={onChange} placeholder={"bio"} />
+            </div>
 
+            <button onClick={onSubmit}>제출</button>
         </div>
     );
 };
