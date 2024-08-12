400d32909125ca032f2fb403d0a20e59a360ce37
ooylf <118517380+yoolf@users.noreply.github.com>
2024-08-12 15:58:07 +0900

    [240812] S12L4까지 수강

--- Section12/src/App.jsx
+++ Section12/src/App.jsx
@@ -1,17 +1,33 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
 
 function App() {
-  return <Routes>
-    <Route path="/" element={<Home></Home>}></Route>
-    <Route path="/new" element={<New></New>}></Route>
-    <Route path="/diary" element={<Diary></Diary>}></Route>
-    <Route path="*" element={<Notfound></Notfound>}></Route>
-  </Routes>;
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/new" element={<New />}></Route>
+        <Route path="/diary/:id" element={<Diary />}></Route>
+        <Route path="*" element={<Notfound />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
